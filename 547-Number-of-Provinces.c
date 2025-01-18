cb392e4435f95b95da169d776069cbe809f4dbce
muhdjasil <mohammedjass007@gmail.com>
2025-01-18 15:13:57 +0530

    [547] [Time Beats: 100.00%] [Memory Beats: 76.31%] - LeetPush

---
+++ 547-Number-of-Provinces.c
@@ -0,0 +1,26 @@
+void bfs(int ** graph , int n , int * visited, int source){
+    int queue[2*n];
+    int front = 0 , rear=0;
+    queue[rear++]=source;
+    visited[source]=1;
+    while(front<rear){
+        int s = queue[front++];
+        for(int i = 0 ; i < n ; i ++){
+            if(!visited[i] && graph[s][i]){
+                visited[i]=1;
+                queue[rear++]=i;
+                }
+        }
+    }
+}
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int * visited = (int *)calloc(isConnectedSize, sizeof(int));
+    int count=0;
+    for(int i = 0 ; i < isConnectedSize ; i ++){
+        if(!visited[i]){
+            bfs(isConnected,isConnectedSize,visited,i);
+            count++;
+        }
+    }
+    return count;
+}
