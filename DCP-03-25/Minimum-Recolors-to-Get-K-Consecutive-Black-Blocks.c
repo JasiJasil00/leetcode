9a3a0d0310265aa17776cb5037e23dd7f3cd8fd9
muhdjasil <mohammedjass007@gmail.com>
2025-03-09 04:11:23 +0530

    [2379] [Time Beats: 100.00%] [Memory Beats: 82.35%] - LeetPush

--- DCP-03-25/Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.c
+++ DCP-03-25/Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks.c
@@ -1,16 +1,6 @@
 int minimumRecolors(char* blocks, int k) {  
     int i = 0;
     int count =0;
-    while( blocks[i]!='\0'){
-        if(blocks[i]=='B' && count < k){
-            count++;
-        }else if(blocks[i] == 'B' && count == k){
-            return 0;
-        }else if(blocks[i] == 'W' ){
-            count=0;
-        }
-        i++;
-    }
     count = 101;
     i = 0;
     while(blocks[i+k-1]!='\0'){
