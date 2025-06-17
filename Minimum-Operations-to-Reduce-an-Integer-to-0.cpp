fb7383b859366976ed4b99fcae848818e8cfa746
muhdjasil <mohammedjass007@gmail.com>
2025-06-17 16:02:40 +0530

    [2571] [Time Beats: 100.00%] [Memory Beats: 83.56%] - LeetPush

--- Minimum-Operations-to-Reduce-an-Integer-to-0.cpp
+++ Minimum-Operations-to-Reduce-an-Integer-to-0.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int a=0;
-        if(n==1)return 1;
-        while(n != 0){
-            int mindiff = INT_MAX;
-            int power;
-            
-            for(int i = 0 ; i < 63 ; i++){
-                int p = 1 << i;
-                int diff = abs(p - n);
-                if( diff < mindiff ){
-                    mindiff = diff;
-                    power = p;
-                }else{
-                    break;
-                }
-            }
-            if(n == 1){
-                a++;
-                break;
-            }
-            if( power > n ){
-                a++;
-                n= power - n;
-            }else{
-                n = n - power;
-                a++;
-            }
-        }
-        return a;
+       if(n == 0 || n ==1)return n;
+
+       int val = 1;
+       while(val*2 < n){
+        val*=2;
+       }
+
+       return 1 + min(minOperations(val*2-n),minOperations(n-val));
     }
 };
