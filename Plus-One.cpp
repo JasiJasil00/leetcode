993bbcdf2846816a3cd3fd1d8d05fe673d32afa4
muhdjasil <mohammedjass007@gmail.com>
2025-06-17 14:17:44 +0530

    [66] [Time Beats: 100.00%] [Memory Beats: 34.75%] - LeetPush

--- Plus-One.cpp
+++ Plus-One.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int size = digits.size();
-        vector<int> ans(size+1);
-        digits[size-1]+=1;
-        if(digits[size-1]>=10)
-        { 
-            for( int i = size-1 ; i>0 ; i --){
-                if(digits[i]>=10){
-                    digits[i]-=10;
-                    digits[i-1]+=1;
-                    ans[i+1]=digits[i];
-                }else{
-                    break;
-                }
+        int n = digits.size();
+        for(int i = n-1 ; i >=0 ; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
             }
+            digits[i]=0;
         }
-        if(digits[0]>=10){
-            ans[0]=1;
-            ans[1]=digits[0]-10;
-            return ans;
-        }
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
