650ef23c69c17dfdf8cd29ca2367a59b720ebbdd
muhdjasil <mohammedjass007@gmail.com>
2025-06-25 04:18:32 +0530

    [80] [Time Beats: 5.16%] [Memory Beats: 55.76%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array-II.cpp
+++ Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -4,8 +4,8 @@ public:
         if(nums.size()<=2) return nums.size();
         int left =0;
         int right =1;
-        while(right!=nums.size()){
-            if(nums[left]!=nums[right]){
+        while(right<=nums.size()){
+            if(right==nums.size()||nums[left]!=nums[right]){
                 if(right-1-2 >= left){
                     nums.erase(nums.begin()+left,nums.begin()+right-2);
                     right = left+ 2;
@@ -13,9 +13,6 @@ public:
                 left=right;
             }
             right++;
-        }
-        if(right-1-1 > left){
-                    nums.erase(nums.begin()+left,nums.begin()+right-2);
         }
         return nums.size();
     }
