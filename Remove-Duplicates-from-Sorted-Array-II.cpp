0cf229838c0092d0c81dff7af198c0b4bc498900
muhdjasil <mohammedjass007@gmail.com>
2025-06-25 04:24:47 +0530

    [80] [Time Beats: 73.74%] [Memory Beats: 79.47%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array-II.cpp
+++ Remove-Duplicates-from-Sorted-Array-II.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()<=2) return nums.size();
-        int left =0;
-        int right =1;
-        while(right<=nums.size()){
-            if(right==nums.size()||nums[left]!=nums[right]){
-                if(right-1-2 >= left){
-                    nums.erase(nums.begin()+left,nums.begin()+right-2);
-                    right = left+ 2;
-                }
-                left=right;
-            }
-            right++;
+       int n = nums.size();
+       if(n<=2)return n;
+       int index = 2;
+       for(int i = 2 ; i < n ; i ++ ){
+        if(nums[i]!=nums[index-2]){
+            nums[index]=nums[i];
+            index++;
         }
-        return nums.size();
+       }
+        return index;
     }
 };
