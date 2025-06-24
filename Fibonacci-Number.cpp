3e2f6986c8677929625e397f48fad0e1f24e1175
muhdjasil <mohammedjass007@gmail.com>
2025-06-24 22:32:14 +0530

    [509] [Time Beats: 52.86%] [Memory Beats: 64.07%] - LeetPush

--- Fibonacci-Number.cpp
+++ Fibonacci-Number.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    std::unordered_map<int, int> dp;
     int fib(int n) {
-        dp[0] = 0;
-        dp[1] = 1;
-        if(dp.contains(n)) return dp[n];
-        dp[n] = fib(n - 1) + fib(n - 2);
-        return dp[n];
+        if(n==0)return 0;
+        if(n==1)return 1;
+        return fib(n-1)+fib(n-2);
     }
 };
