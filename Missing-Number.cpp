502eaf0c50fb7b7318f63693b22476b783f4a13d
muhdjasil <mohammedjass007@gmail.com>
2025-06-25 23:34:36 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 38.51%] - LeetPush

--- Missing-Number.cpp
+++ Missing-Number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        std:: unordered_map<int,int>hash;
+        int sum = 0;
         int n = nums.size();
         for(int i = 0 ; i < n ; i++){
-            hash[nums[i]]=1;
+            sum+=nums[i];
         }
-        for(int i = 0 ; i <= n ; i ++){
-            if(hash[i]==0)return i;
-        }
-        return 0;
+        int y = (n*(n+1))/2;
+        return y-sum;
     }
 };
