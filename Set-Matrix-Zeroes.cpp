437055405a736c8c233d1e85dbd50518f6b590d5
muhdjasil <mohammedjass007@gmail.com>
2025-06-26 19:32:25 +0530

    [73] [Time Beats: 100.00%] [Memory Beats: 50.92%] - LeetPush

--- Set-Matrix-Zeroes.cpp
+++ Set-Matrix-Zeroes.cpp
@@ -4,22 +4,34 @@ public:
         int row = matrix.size();
         if(row==0)return;
         int col = matrix[0].size();
-        vector <int> r(row,0);
-        vector <int> c(col,0);
+        // vector <int> r(row,0);
+        // vector <int> c(col,0);
+        int col0=1;
         for(int i = 0 ; i < row ; i ++){
             for(int j = 0 ; j < col ; j++ ){
                 if(matrix[i][j]==0){
-                    r[i]=1;
-                    c[j]=1;
+                matrix[i][0]=0;
+                if(j!=0)
+                matrix[0][j]=0;
+                else
+                    col0=0;
                 }
             }
         }
-        for(int i = 0 ; i < row ; i ++){
-            for(int j = 0 ; j < col ; j++ ){
-                if( r[i]==1 || c[j]==1){
+        for(int i = 1 ; i < row ; i ++){
+            for(int j = 1 ; j < col ; j++ ){
+                if( matrix[i][0]==0 || matrix[0][j]==0){
                    matrix[i][j]=0;
                 }
             }
+        }
+        if(matrix[0][0]==0){
+            for(int j =0 ; j < col ; j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0 == 0){
+            for(int i =0 ; i < row ; i++)matrix[i][0]=0;
         }
     }
 };
