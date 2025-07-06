395ffb4c68fd51964cd22435c16cba4e2e9faf7f
muhdjasil <mohammedjass007@gmail.com>
2025-07-07 03:41:59 +0530

    [628] [Time Beats: 65.55%] [Memory Beats: 72.83%] - LeetPush

--- Maximum-Product-of-Three-Numbers.cpp
+++ Maximum-Product-of-Three-Numbers.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        
-        int product1 = nums[n-1] * nums[n-2] * nums[n-3];           // top 3 largest
-        int product2 = nums[0] * nums[1] * nums[n-1];               // 2 smallest * largest
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
 
-        return max(product1, product2);
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+
+        return max(max1 * max2 * max3, max1 * min1 * min2);
     }
 };
