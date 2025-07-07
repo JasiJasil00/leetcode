d56774ce8aa7b1454bd1c10b71d15c41f6eef5be
muhdjasil <mohammedjass007@gmail.com>
2025-07-07 18:10:21 +0530

    [231] [Time Beats: 100.00%] [Memory Beats: 53.16%] - LeetPush

--- Power-of-Two.cpp
+++ Power-of-Two.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int sum = 0;
-         if(n==INT_MIN)return false;
-        while(sum < 32){
-            int p = 1<<sum;
-            if(n == p )return true;
-            sum++;
-        }
+        if(n==0)return false;
+        if(n==INT_MIN)return false;
+        n= n & (n-1);
+        if(n==0)return true;
         return false;
     }
 };
