780faba340aa498e2d0cd446dfbe067e3018114d
muhdjasil <mohammedjass007@gmail.com>
2025-07-07 19:36:04 +0530

    [242] [Time Beats: 39.48%] [Memory Beats: 26.65%] - LeetPush

--- Valid-Anagram.cpp
+++ Valid-Anagram.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<int,int> hash,hash2;
-        if(s.size()!=t.size())return false;
-        int n = s.size();
-        for(int i = 0; i < n ; i ++){
-            hash[s[i]]++;
-            hash2[t[i]]++;
-        }
-        for(int i = 0; i < n ; i ++){
-           if(hash[s[i]]!=hash2[s[i]])return false;
-        }
+        if(s.size()!=t.size())
+            return false;
+        unordered_map<char,int>count;
+        for(char c:s) count[c]++;
+        for(char c:t)
+            if(--count[c]<0) return false;
         return true;
     }
 };
