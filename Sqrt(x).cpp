62034f31ad6b424257f99ca68fd7351eb09e2b89
muhdjasil <mohammedjass007@gmail.com>
2025-07-07 14:10:52 +0530

    [69] [Time Beats: 100.00%] [Memory Beats: 50.95%] - LeetPush

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long l = 0 , r = x, m;
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(m*m==x)return m;
+            else if(m*m>x)r=m-1;
+            else l =m+1;
+        }
+        if(m*m>x)m--;
+        return m;
     }
 };
