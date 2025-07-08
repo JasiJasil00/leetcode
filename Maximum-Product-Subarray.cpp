906d3931783648c678943e602b2d7e5d9f36d40c
muhdjasil <mohammedjass007@gmail.com>
2025-07-08 15:58:29 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 20.96%] - LeetPush

--- Maximum-Product-Subarray.cpp
+++ Maximum-Product-Subarray.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        long long product;
+        long long product = 1;
         long long max = INT_MIN;
         int n = nums.size();
         for(int i = 0 ; i < n ; i++ ){
-            product=nums[i];
-            max=std::max(product,max);
-            for(int j = i+1 ; j < n ; j++){
-                product*=nums[j];
-                max=std::max(product,max);
-            }
+            product*=nums[i];
+            max = std::max(max,product);
+            if(product==0)product=1;
+        }
+        product=1;
+        for(int i = n-1 ; i >=0 ; i--){
+            product*=nums[i];
+            max = std::max(max,product);
+            if(product==0)product=1;
         }
         return max;
     }
