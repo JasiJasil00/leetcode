da023a365b2ffeb35595734b46db797c70a092d8
muhdjasil <mohammedjass007@gmail.com>
2025-07-28 19:15:06 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 79.79%] - LeetPush

--- Binary-Search.cpp
+++ Binary-Search.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
+    int recursive(vector<int> &nums, int target, int low, int high){
+        if(low>high)return -1;
+        int mid = (low+high)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target) return recursive(nums,target,low,mid-1);
+        else return recursive(nums,target,mid+1,high);
 
+    }
     int search(vector<int>& nums, int target) {
-             int low = 0, high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-
-        return -1;
+        return recursive(nums,target,0,nums.size()-1);
     }
 };
