a997fac692b415886c8bddb8d4d3ba5e873e3743
muhdjasil <mohammedjass007@gmail.com>
2025-07-30 21:17:09 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 32.20%] - LeetPush

---
+++ Find-Peak-Element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size()==1) return 0;
+        if(nums.size()==2) return (nums[0]>nums[1])? 0 : 1;
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
+        while(low <= high){
+            mid =low + (high - low)/2;
+            if(mid>0 && mid < nums.size()-1 && nums[mid]<nums[mid-1] && nums[mid] > nums[mid+1] ){
+                high = mid-1;
+            }else if(mid>0 && mid < nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid] < nums[mid+1]){
+                low = mid +1;
+            }else if(mid>0 && mid < nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else{
+                if(high - low == 1){
+                    if(nums[high]>nums[low]){
+                        return high;
+                    }else{
+                        return low;
+                    }
+                }else{
+                    low = mid+1;
+                }
+            }
+        }
+        return mid;
+    }
+};
