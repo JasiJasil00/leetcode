e0284dddc933e52c388dd64c517eb16e4d29fc5f
muhdjasil <mohammedjass007@gmail.com>
2025-08-05 22:59:56 +0530

    [1011] [Time Beats: 67.89%] [Memory Beats: 68.00%] - LeetPush

---
+++ Capacity-To-Ship-Packages-Within-D-Days.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool possible(vector<int>& weights, int days, int k) {
+        int total = 0;
+        int m = 0;
+         for (int i = 0; i < weights.size(); i++) {
+            total += weights[i];
+            if (total > k) {
+                total = 0;
+                m++ ;
+                total += weights[i];
+            }
+        }
+        if(total!=0)m++;
+        if(total>k)m++;
+        return m <= days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int max = INT_MIN;
+        for (int i = 0; i < weights.size(); i++) {
+            if (max < weights[i])
+                max = weights[i];
+        }
+        if (days >= weights.size())
+            return max;
+        int low = max;
+        int high = max * weights.size();
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (possible(weights,days,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
