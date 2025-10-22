e03e34ca0565b348248911b4f1e420f18fb7c900
muhdjasil <mohammedjass007@gmail.com>
2025-10-22 05:57:11 +0530

    [72] [Time Beats: 34.96%] [Memory Beats: 49.22%] - LeetPush

--- Edit-Distance.cpp
+++ Edit-Distance.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int f(string &word1,string &word2,int i, int j,vector<vector<int>>&dp){
-        if(i < 0)return j+1;
-        if(j < 0)return i+1;
-        if(dp[i][j]!=-1)return dp[i][j]; 
-        if(word1[i]==word2[j])return dp[i][j]=f(word1,word2,i-1,j-1,dp);
-        int number = min(f(word1,word2,i-1,j,dp),f(word1,word2,i,j-1,dp));
-        dp[i][j]= 1 + min(number,f(word1,word2,i-1,j-1,dp));
-        return dp[i][j];
-    }
+    
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        return f(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+        for(int i = 0 ; i <= word1.size() ; i ++)dp[i][0]=i;
+        for(int j = 0 ; j <= word2.size() ; j++ )dp[0][j]=j;
+
+         for(int i = 1 ; i <= word1.size() ; i ++){
+            for(int j = 1 ; j <= word2.size() ; j++ ){
+               if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min({dp[i - 1][j],     // delete
+                                        dp[i][j - 1],     // insert
+                                        dp[i - 1][j - 1]  // replace
+                                       });
+
+                }
+            }
+         
+        return dp[word1.size()][word2.size()];
     }
 };
