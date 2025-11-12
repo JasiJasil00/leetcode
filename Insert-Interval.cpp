fefcfab8c025cb2fe8dea0b8550da5f45634d90f
muhdjasil <mohammedjass007@gmail.com>
2025-11-12 21:42:18 +0530

    [57] [Time Beats: 100.00%] [Memory Beats: 74.58%] - LeetPush

--- Insert-Interval.cpp
+++ Insert-Interval.cpp
@@ -9,15 +9,11 @@ public:
             result.push_back({intervals[i][0],intervals[i][1]});
             i++;   
         }
-        if(i < intervals.size() && intervals[i][0]<=start){
-            start = intervals[i][0];
-        }
-        while( i < intervals.size() && intervals[i][0] <= end){
-            i++;
-        }
-        if(i > 0 && i-1 < intervals.size() && intervals[i-1][1] >= end ){
-            end = intervals[i-1][1];
-        }
+        while (i < intervals.size() && intervals[i][0] <= end) {
+    start = min(start, intervals[i][0]);
+    end   = max(end, intervals[i][1]);
+    i++;
+}
          result.push_back({start,end});
          while (i < intervals.size()) {
             result.push_back(intervals[i]);
