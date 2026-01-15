bb604e23757fb897a70d08785121947209206d71
muhdjasil <mohammedjass007@gmail.com>
2026-01-15 10:04:27 +0530

    [175] [Time Beats: 23.75%] - LeetPush

--- Combine-Two-Tables.sql
+++ Combine-Two-Tables.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
-select e.firstName,e.lastName,d.city,d.state
-from Person e 
-LEFT JOIN Address d ON e.personId=d.personId
+1# Write your MySQL query statement below
+2select e.firstName,e.lastName,d.city,d.state
+3from Person e 
+4left JOIN Address d 
+5ON e.personId=d.personId
