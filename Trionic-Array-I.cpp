cf4f46cff92f1acdb34353edc98018d7f4d85536
muhdjasil <mohammedjass007@gmail.com>
2026-02-03 17:51:13 +0530

    [3637] [Time Beats: 100.00%] [Memory Beats: 69.85%] - LeetPush

--- Trionic-Array-I.cpp
+++ Trionic-Array-I.cpp
@@ -1,20 +1,25 @@
 1class Solution {
 2public:
 3    bool isTrionic(vector<int>& nums) {
-4        int n = nums.size();
-5        if(n < 4)return false;
-6        int i = 0;
-7        while(i+1 < n && nums[i] < nums[i+1])i++;
-8        if(i==0) return false;
-9
-10        int decstart = i;
-11        while(i+1 < n && nums[i] > nums[i+1])i++;
-12        if(i==decstart)return false;
-13
-14        int start = i;
-15        while(i+1 < n && nums[i] < nums[i+1])i++;
-16        if(i==start)return false;
-17
-18        return i==n-1;
-19    }
-20};
+4        int q = 1;
+5        int n = nums.size();
+6        bool statement;
+7        int index = 0;
+8        for(int i = 0 ; i < n-1 ; i++){
+9            if(q%2==1){
+10                statement = nums[i] < nums[i+1];
+11            }else{
+12                statement = nums[i] > nums[i+1];
+13            }
+14            if(!statement && i!=index){
+15                q++;
+16                index = i;
+17                i--;
+18            }
+19            else if(!statement)return false;
+20            if( q > 3)return false;
+21        }
+22        if(q < 3)return false;
+23        return true;
+24    }
+25};
