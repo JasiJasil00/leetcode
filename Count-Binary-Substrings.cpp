1a9ae03a2395442ad5b4f5c404a37bb5e970b633
muhdjasil <mohammedjass007@gmail.com>
2026-02-19 21:21:39 +0530

    [696] [Time Beats: 67.83%] [Memory Beats: 82.66%] - LeetPush

--- Count-Binary-Substrings.cpp
+++ Count-Binary-Substrings.cpp
@@ -1,19 +1,19 @@
 1class Solution {
 2public:
 3    int countBinarySubstrings(string s) {
-4         vector<int> groups;
-5         groups.push_back(1);
-6          for(int i = 1; i < s.size(); i++){
-7            if(s[i] == s[i-1]) {
-8                groups.back()++;
-9            } else {
-10                groups.push_back(1);
-11            }
-12        }
-13         int ans = 0;
-14         for(int i = 0 ; i < groups.size()-1 ; i++){
-15            ans+=std::min(groups[i],groups[i+1]);
-16         }
-17         return ans;
+4         int res =0;
+5         int countlen = 1;
+6         int prevlen = 0;
+7         for (int i = 0 ; i < s.size()-1 ; i++){
+8            if(s[i]==s[i+1]){
+9                countlen++;
+10            }else{
+11                res+=min(countlen,prevlen);
+12                prevlen=countlen;
+13                countlen=1;
+14            }
+15         }
+16         res+=min(countlen,prevlen);
+17         return res;
 18    }
 19};
