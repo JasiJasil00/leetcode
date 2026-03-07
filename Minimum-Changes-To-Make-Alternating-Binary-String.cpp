dc2fead26edbfce8308cb32e969e5504180bd8fe
muhdjasil <mohammedjass007@gmail.com>
2026-03-07 13:17:23 +0530

    [1758] [Time Beats: 2.42%] [Memory Beats: 74.61%] - LeetPush

--- Minimum-Changes-To-Make-Alternating-Binary-String.cpp
+++ Minimum-Changes-To-Make-Alternating-Binary-String.cpp
@@ -1,23 +1,30 @@
 1class Solution {
 2public:
 3    int minOperations(string s) {
-4        char c = s[0];
-5        int count =0;
-6        if(c=='1'){
-7                c='0';
-8            }else{
-9                c='1';
-10            }
-11        for(int i = 1 ; i < s.size() ; i++){
-12            if(s[i]!=c){
-13                count++;
-14            }
-15            if(c=='1'){
-16                c='0';
-17            }else{
-18                c='1';
-19            }
-20        }
-21        return (count > s.size()/2) ? s.size()-count : count;
-22    }
-23};
+4       int k = 0;
+5       int min = INT_MAX;
+6       while(k!=s.size()){
+7         char c = s[0];
+8        int count =0;
+9        if(c=='1'){
+10                c='0';
+11            }else{
+12                c='1';
+13            }
+14        for(int i = 1 ; i < s.size() ; i++){
+15            if(s[i]!=c){
+16                count++;
+17            }
+18            if(c=='1'){
+19                c='0';
+20            }else{
+21                c='1';
+22            }
+23        }
+24        int l = (count > s.size()/2) ? s.size()-count : count;
+25        min = std::min(l,min);
+26        k++;
+27       }
+28       return min;
+29    }
+30};
