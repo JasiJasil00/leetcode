fe15adeccb99c39977d480b493c415e5e06a7851
muhdjasil <mohammedjass007@gmail.com>
2026-05-05 07:40:58 +0530

    [61] [Time Beats: 100.00%] [Memory Beats: 31.20%] - LeetPush

--- Rotate-List.cpp
+++ Rotate-List.cpp
@@ -11,25 +11,27 @@
 11class Solution {
 12public:
 13    ListNode* rotateRight(ListNode* head, int k) {
-14        if(head==NULL || head->next==NULL)return head;
-15        int n = 0;
-16        ListNode * temp = head;
-17        ListNode * end=NULL;
+14        if(head == NULL || head->next == NULL || k == 0)
+15    return head;
+16        int length=0;
+17        ListNode* temp = head;
 18        while(temp!=NULL){
-19            end=temp;
+19            length++;
 20            temp=temp->next;
-21            n++;
-22        }
-23        if(n!=0)k=k%n;
-24        int i = 0;
-25        temp = head;
-26        while(i < n-k-1){
-27            temp=temp->next;
-28            i++;
-29        }
-30        end->next=head;
-31        head = temp->next;
-32        temp->next=NULL;
-33        return head;
-34    }
-35};
+21        }
+22        k = k % length;
+23        temp = head;
+24        for(int i = 0; i < length-k-1 ; i++){
+25            temp = temp->next;
+26        }
+27        ListNode * start = temp->next;
+28        temp->next = NULL;
+29        temp = start;
+30            if(temp==NULL)return head;
+31        while(temp->next!=NULL){
+32            temp=temp->next;
+33        }
+34        temp->next=head;
+35        return start;
+36    }
+37};
