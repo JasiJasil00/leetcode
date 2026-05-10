4f2395b018518486bcbfe06b2c1f7b4353936252
muhdjasil <mohammedjass007@gmail.com>
2026-05-10 15:09:46 +0530

    [42] [Time Beats: 100.00%] [Memory Beats: 35.10%] - LeetPush

--- Trapping-Rain-Water.cpp
+++ Trapping-Rain-Water.cpp
@@ -2,30 +2,23 @@
 2public:
 3    int trap(vector<int>& height) {
 4        int n = height.size();
-5        int left = 0;
-6        int right = n-1;
-7        int maxleft = -1;
-8        int maxright= -1;
-9        int sum = 0;
-10        while(left<=right){
-11            if(height[left]<height[right]){
-12               if(height[left] >= maxleft){
-13                    maxleft = height[left];
-14                }else{
-15                    sum += maxleft - height[left];
-16                }
-17
-18                left++;
-19            }else{
-20                 if(height[right] >= maxright){
-21                    maxright = height[right];
-22                }else{
-23                    sum += maxright - height[right];
-24                }
-25
-26                right--;
-27            }
-28        }
-29        return sum;      
-30    }
-31};
+5        stack<int>s;
+6        int sum = 0;
+7        for(int i = 0 ; i < n ; i++){
+8            while(!s.empty() && height[i] > height[s.top()]){
+9                int bottom = s.top();
+10                s.pop();
+11                if(s.empty())break;
+12                int left = s.top();
+13                int width = i - left-1;
+14                  int boundedHeight =
+15                    min(height[left], height[i]) - height[bottom];
+16
+17                sum += width * boundedHeight;
+18            }
+19            s.push(i);
+20        }
+21        return sum;
+22          
+23    }
+24};
