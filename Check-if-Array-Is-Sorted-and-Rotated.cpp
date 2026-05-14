61e3afb39ff706e8600f23995ba4a6db7ebfe34e
muhdjasil <mohammedjass007@gmail.com>
2026-05-14 14:02:15 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 53.28%] - LeetPush

--- Check-if-Array-Is-Sorted-and-Rotated.cpp
+++ Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -1,13 +1,15 @@
-class Solution {
-public:
-    bool check(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > nums[(i + 1) % n]) {
-                count++;
-            }
-        }
-        return count <= 1;
-    }
-};
+1class Solution {
+2public:
+3    bool check(vector<int>& nums) {
+4        int i ;
+5        for(i = 1 ; i < nums.size(); i++){
+6            if(nums[i-1]>nums[i])break;
+7        }
+8        if(i==nums.size())return true;
+9        int n = nums.size();
+10        for(int k = 0 ; k < n-1 ; k++ ){
+11            if(nums[(i+k)%n]>nums[(i+k+1)%n])return false;
+12        }
+13        return true;
+14    }
+15};
