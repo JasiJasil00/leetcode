122c71f0202989218668767f8be50231171c4bf4
muhdjasil <mohammedjass007@gmail.com>
2026-05-23 12:08:31 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 77.46%] - LeetPush

--- Sort-Colors.cpp
+++ Sort-Colors.cpp
@@ -1,20 +1,21 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int mid = 0;
-        int low = 0;
-        int high = nums.size()-1;
-        while(mid<high+1){
-            if(nums[mid]==0){
-                swap(nums[mid],nums[low]);
-                mid++;
-                low++;
-            }else if(nums[mid]==1){
-                mid++;
-            }else{
-                swap(nums[mid],nums[high]);
-                high--;
-            }
-        }
-    }
-};
+1class Solution {
+2public:
+3    void sortColors(vector<int>& nums) {
+4        int n = nums.size();
+5        int low = 0;
+6        int mid = 0;
+7        int high = n-1;
+8        while(mid <= high){
+9            if(nums[mid]==0){
+10                swap(nums[low],nums[mid]);
+11                mid++;
+12                low++;
+13            }
+14            else if(nums[mid]==1)mid++;
+15            else if(nums[mid]==2){
+16                swap(nums[mid],nums[high]);
+17                high--;
+18            }
+19        }
+20    }
+21};
