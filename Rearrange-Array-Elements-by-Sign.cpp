0be27335e996eb13805bdd3999a1778ca211d055
muhdjasil <mohammedjass007@gmail.com>
2026-05-25 13:51:01 +0530

    [2149] [Time Beats: 100.00%] [Memory Beats: 77.81%] - LeetPush

--- Rearrange-Array-Elements-by-Sign.cpp
+++ Rearrange-Array-Elements-by-Sign.cpp
@@ -1,18 +1,21 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> a(nums.size(),0);
-        int neg = 1;
-        int pos = 0;
-        for(int i = 0 ; i < nums.size() ; i ++){
-            if(nums[i]<0){
-                 a[neg] = nums[i];
-    neg += 2;
-            }else{
-               a[pos] = nums[i];
-    pos += 2;
-            }
-        }
-        return a;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> rearrangeArray(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int>ans(n,0);
+6        int neg = 1;
+7        int pos = 0;
+8        for(int i = 0 ; i < n ; i ++){
+9            if(nums[i] < 0){
+10                ans[neg]=nums[i];
+11                neg+=2;
+12                
+13            }else{
+14                ans[pos]=nums[i];
+15                pos+=2;
+16            }
+17        }
+18        return ans;
+19        
+20    }
+21};
