80184613c82b0d791ea42d37d95e31fabc8c21d5
muhdjasil <mohammedjass007@gmail.com>
2026-05-25 14:57:39 +0530

    [31] [Time Beats: 100.00%] [Memory Beats: 32.22%] - LeetPush

--- Next-Permutation.cpp
+++ Next-Permutation.cpp
@@ -1,24 +1,23 @@
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int m = -1;
-        for(int i = n-1 ; i >=1 ; i--){
-                if(nums[i-1]<nums[i]){
-                    m=i-1;
-                    break;
-                }
-        }
-        if(m==-1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        for(int i = n-1 ; i >= 1 ; i--){
-            if(nums[i]>nums[m]){
-                swap(nums[i],nums[m]);
-                break;
-            }
-        }
-        reverse(nums.begin()+m+1,nums.end());
-    }
-};
+1class Solution {
+2public:
+3    void nextPermutation(vector<int>& nums) {
+4        int n = nums.size();
+5        int i;
+6        for( i = n-2 ; i >=0 ; i--){
+7            if(nums[i]<nums[i+1])break;
+8        }
+9        if(i==-1){
+10            reverse(nums.begin(),nums.end());
+11            return;
+12        }
+13        int index = i;
+14        for (int i = nums.size() - 1; i > index; i--) {
+15            // Swap the two
+16            if (nums[i] > nums[index]) {
+17                swap(nums[i], nums[index]);
+18                break;
+19            }
+20        }
+21        reverse(nums.begin()+index+1,nums.end());
+22    }
+23};
