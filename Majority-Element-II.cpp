935a4c8d3fed3708f514d0ba765fc6ccda455120
muhdjasil <mohammedjass007@gmail.com>
2026-06-09 15:39:01 +0530

    [229] [Time Beats: 41.66%] [Memory Beats: 34.35%] - LeetPush

--- Majority-Element-II.cpp
+++ Majority-Element-II.cpp
@@ -1,16 +1,17 @@
-class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        vector<int> result;
-        int n = nums.size();
-
-        for (int num : nums) {
-            freq[num]++;
-            if(freq[num] > n/3 && (result.size()==0 || result[0]!=num))result.push_back(num);
-            if(result.size()==2)break;
-        }
-
-        return result;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> majorityElement(vector<int>& nums) {
+4        int n = nums.size();
+5        unordered_map<int,int> hash;
+6        vector<int> result;
+7        for(int i = 0 ; i < nums.size() ; i++){
+8            hash[nums[i]]++;
+9        }
+10        for(auto & s : hash){
+11            if(s.second>n/3){
+12                result.push_back(s.first);
+13            }
+14        }
+15        return result;
+16    }
+17};
