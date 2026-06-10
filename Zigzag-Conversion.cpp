8c91cd30a0dc48765a509b7d252af6d088e0903d
muhdjasil <mohammedjass007@gmail.com>
2026-06-10 16:52:13 +0530

    [6] [Time Beats: 84.68%] [Memory Beats: 84.58%] - LeetPush

--- Zigzag-Conversion.cpp
+++ Zigzag-Conversion.cpp
@@ -8,13 +8,13 @@
 8       int i = k;
 9       string inter="";
 10       while(i < n){
-11            int index = i-k+numRows+numRows-2-k; 
-12              if(i<n){
-13                  inter+=s[i];
-14                  i+=numRows;
-15              }else break;
-16              
-17              if(k>=1&&k<numRows-1){
+11              if(i<n){
+12                  inter+=s[i];
+13                  i+=numRows;
+14              }else break;
+15              
+16              if(k>=1&&k<numRows-1){
+17                    int index = i-k+numRows-2-k;
 18                  if(index<n)inter+=s[index];
 19              } 
 20              if(numRows>=2)i+=(numRows-2);    
