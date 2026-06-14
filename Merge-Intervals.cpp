a4dcc82b373d5be25dceca03319c7212c7306c72
muhdjasil <mohammedjass007@gmail.com>
2026-06-14 14:25:33 +0530

    [56] [Time Beats: 65.93%] [Memory Beats: 59.02%] - LeetPush

--- Merge-Intervals.cpp
+++ Merge-Intervals.cpp
@@ -1,16 +1,21 @@
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector <vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        int n = intervals.size();
-        for(int i = 0 ; i < n  ; i ++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
-            }else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-            }
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+4        vector<vector<int>> result;
+5        int n = intervals.size();
+6        sort(intervals.begin(),intervals.end());
+7        int start = intervals[0][0];
+8        int end = intervals[0][1];
+9        for(int i = 1 ; i < n ; i ++){
+10            if(end >= intervals[i][0] && end < intervals[i][1] ){
+11                end = intervals[i][1];
+12            }else if(end < intervals[i][0] ){
+13                result.push_back({start,end});
+14                start = intervals[i][0];
+15                end = intervals[i][1];
+16            }
+17        }
+18        result.push_back({start,end});
+19        return result;
+20    }
+21};
