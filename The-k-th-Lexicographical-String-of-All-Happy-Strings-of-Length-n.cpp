16622af94014b599038ef7bda5c923af3059b8f0
muhdjasil <mohammedjass007@gmail.com>
2026-06-16 21:17:04 +0530

    [1415] [Time Beats: 100.00%] [Memory Beats: 55.08%] - LeetPush

--- The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
+++ The-k-th-Lexicographical-String-of-All-Happy-Strings-of-Length-n.cpp
@@ -1,38 +1,24 @@
 1class Solution {
 2public:
-3    string happy(char a,int n,int k, int& i){
-4        if(n==0){
-5            i++;
-6            return "";
-7        }
-8        string r="";
-9        if(a=='a'){
-10            r= 'b' + happy('b',n-1,k,i);
-11            if(i==k)return r;
-12            r= 'c' + happy('c',n-1,k,i);
-13            if(i==k)return r;
-14        }else if(a=='b'){
-15             r= 'a' + happy('a',n-1,k,i);
-16            if(i==k)return r;
-17            r= 'c' + happy('c',n-1,k,i);
-18            if(i==k)return r;
-19        }else if(a=='c'){
-20             r= 'a' + happy('a',n-1,k,i);
-21            if(i==k)return r;
-22            r= 'b' + happy('b',n-1,k,i);
-23            if(i==k)return r;
-24        }else{
-25            r= 'a' + happy('a',n-1,k,i);
-26            if(i==k)return r;
-27             r= 'b' + happy('b',n-1,k,i);
-28            if(i==k)return r;
-29            r= 'c' + happy('c',n-1,k,i);
-30            if(i==k)return r;
-31        }
-32        return "";
-33    }
-34    string getHappyString(int n, int k) {
-35        int pos = 0;
-36       return happy('*',n,k,pos);
-37    }
-38};
+3    string dfs(string curr, int n , int k ,int& count){
+4        if(curr.size()==n){
+5            count++;
+6            if(count==k)return curr;
+7            return "";
+8        }
+9        for(auto c : {'a', 'b', 'c'})
+10        {
+11            if(curr == "" or c != curr.back())
+12            {
+13                string res = dfs(curr+c, n, k, count);
+14                if(!res.empty()) return res;
+15            }
+16        }
+17        return "";
+18    }
+19    string getHappyString(int n, int k) {
+20        int count = 0;
+21        return dfs("",n,k,count); 
+22        
+23    }
+24};
