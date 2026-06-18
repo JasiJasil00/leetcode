5a4d3c1698ad568c7deb219ea5ea066d207b268a
muhdjasil <mohammedjass007@gmail.com>
2026-06-18 20:58:50 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 42.41%] - LeetPush

--- Search-Insert-Position.cpp
+++ Search-Insert-Position.cpp
@@ -1,19 +1,18 @@
-class Solution {
-public:
-    void recursive(vector<int> &nums, int target, int low, int high,int &ans){
-        if(low>high)return;
-        int mid = (low+high)/2;
-        if(nums[mid]==target){ans = mid;}
-        else if(nums[mid]>target){
-        ans = mid;
-recursive(nums,target,low,mid-1,ans);
-        } 
-        else  recursive(nums,target,mid+1,high,ans);
-
-    }
-    int searchInsert(vector<int>& nums, int target) {
-        int ans=nums.size();
-        recursive(nums,target,0,nums.size()-1,ans);
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    int searchInsert(vector<int>& nums, int target) {
+4        int left = 0;
+5        int right = nums.size()-1;
+6        while(left<=right){
+7            int mid = left + ((right-left)/2);
+8            if(nums[mid]>target){
+9                right=mid-1;
+10            }else if(nums[mid]<target){
+11                left=mid+1;
+12            }else{
+13                return mid;
+14            }
+15        }
+16        return left;
+17    }
+18};
