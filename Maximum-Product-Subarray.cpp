7ab3e6b1cec3af88d59493324c5dc557a7a72d92
muhdjasil <mohammedjass007@gmail.com>
2026-06-18 20:34:33 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 58.84%] - LeetPush

--- Maximum-Product-Subarray.cpp
+++ Maximum-Product-Subarray.cpp
@@ -1,17 +1,18 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int max = INT_MIN;
-        int prefsum = 1;
-        int suffsum =1;
-        for( int i = 0 ; i  < nums.size() ; i++){
-            prefsum*=nums[i];
-            suffsum*=nums[nums.size()-1-i];
-            max = std::max(max,suffsum);
-            max=std::max(max,prefsum);
-            if(prefsum==0)prefsum=1;
-            if(suffsum==0)suffsum=1;
-        }
-        return max;
-    }
-};
+1class Solution {
+2public:
+3    int maxProduct(vector<int>& nums) {
+4        int max = INT_MIN;
+5        int n = nums.size();
+6        int prefixsum = 1;
+7        int suffixsum = 1;
+8        for(int i = 0 ; i < n ; i++){
+9            prefixsum*=nums[i];
+10            suffixsum*=nums[n-1-i];
+11            max = std::max(suffixsum,max);
+12            max = std::max(prefixsum,max);
+13            if(prefixsum==0)prefixsum=1;
+14            if(suffixsum==0)suffixsum=1;
+15        }
+16        return max;
+17    }
+18};
