461b4fb6ccff9d6e2b57dc373c104c7d6064dbe6
muhdjasil <mohammedjass007@gmail.com>
2026-06-19 21:05:37 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 34.00%] - LeetPush

--- Search-in-Rotated-Sorted-Array.cpp
+++ Search-in-Rotated-Sorted-Array.cpp
@@ -1,26 +1,30 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>=target){
-                    
-                     high=mid-1;
-                }else{
-                   low=mid+1;
-                }
-            }else{
-                if(nums[high]>=target && nums[mid]<=target){
-                    low=mid+1;
-                }else{
-                    high=mid-1;
-                }
-            }
-        }
-        return -1;
-    }
-};
+1class Solution {
+2public:
+3    int search(vector<int>& nums, int target) {
+4        int left = 0;
+5        int right = nums.size()-1;
+6        int pos = -1;
+7        while(left<=right){
+8            int mid = left + ((right-left)/2);
+9            if(nums[mid]==target){
+10                return mid;
+11            }
+12            if(nums[left]<=nums[mid]){
+13                if(nums[mid]>target && nums[left]<=target){
+14                    right = mid-1;
+15                }else{
+16                    left=mid+1;
+17                }
+18            }else{
+19                if(nums[right]<target){
+20                    right=mid-1;
+21                }else if(nums[mid]>target){
+22                    right=mid-1;
+23                }else{
+24                    left = mid+1;
+25                }
+26            }
+27        }
+28        return pos;
+29    }
+30};
