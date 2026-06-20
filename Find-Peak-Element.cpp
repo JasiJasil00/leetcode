c5f566234bb6b2bf1febc7259a4761787bb20ddc
muhdjasil <mohammedjass007@gmail.com>
2026-06-20 21:30:19 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 71.45%] - LeetPush

--- Find-Peak-Element.cpp
+++ Find-Peak-Element.cpp
@@ -1,31 +1,19 @@
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        if(nums.size()==1) return 0;
-        int low = 0;
-        int high = nums.size()-1;
-        int mid;
-        while(low <= high){
-            mid =low + (high - low)/2;
-            if(mid>0 && mid < nums.size()-1 && nums[mid]<nums[mid-1] && nums[mid] > nums[mid+1] ){
-                high = mid-1;
-            }else if(mid>0 && mid < nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid] < nums[mid+1]){
-                low = mid +1;
-            }else if(mid>0 && mid < nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid] > nums[mid+1]){
-                return mid;
-            }
-            else{
-                if(high - low == 1){
-                    if(nums[high]>nums[low]){
-                        return high;
-                    }else{
-                        return low;
-                    }
-                }else{
-                    low = mid+1;
-                }
-            }
-        }
-        return mid;
-    }
-};
+1class Solution {
+2public:
+3    int findPeakElement(vector<int>& nums) {
+4        int n = nums.size();
+5        int low = 0;
+6        int high = n-1;
+7        while(low<=high){
+8            int mid = low + ((high-low)/2);
+9            if( mid < n-1 && nums[mid]<=nums[mid+1]){
+10                low=mid+1;
+11            }else if(mid>=1 && nums[mid]<nums[mid-1]){
+12                high= mid-1;
+13            }else{
+14                return mid;
+15            }
+16        }
+17        return (nums[n-1]>nums[n-2])? nums[n-1] : nums[0];
+18    }
+19};
