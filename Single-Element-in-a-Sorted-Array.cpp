69b3886b51d1668e5c19f54e0b02f78abf483514
muhdjasil <mohammedjass007@gmail.com>
2026-06-20 21:23:18 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 33.78%] - LeetPush

--- Single-Element-in-a-Sorted-Array.cpp
+++ Single-Element-in-a-Sorted-Array.cpp
@@ -1,31 +1,29 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-        int mid;
-        while (low <= high) {
-            mid = low + (high - low) / 2;
-            if (mid % 2 == 0) {
-                if (mid > 0 && nums[mid] == nums[mid - 1]) {
-                    high = mid - 1;
-                } else if (mid < nums.size() - 1 &&
-                           nums[mid] == nums[mid + 1]) {
-                    low = mid + 1;
-                } else {
-                    return nums[mid];
-                }
-            }else{
-                 if (mid > 0 && nums[mid] == nums[mid - 1]) {
-                    low = mid + 1;
-                } else if (mid < nums.size() - 1 &&
-                           nums[mid] == nums[mid + 1]) {
-                    high = mid - 1;
-                } else {
-                    return nums[mid];
-                }
-            }
-        }
-        return mid;
-    }
-};
+1class Solution {
+2public:
+3    int singleNonDuplicate(vector<int>& nums) {
+4       int low  = 0 ;
+5       int n = nums.size();
+6       int high = n-1;
+7       while(low <= high){
+8        int mid = low + ((high-low)/2);
+9            if(mid%2==0){
+10                if(mid>=1 && nums[mid]==nums[mid-1]){
+11                    high=mid-1;
+12                }else if(mid<n-1 && nums[mid]==nums[mid+1]){
+13                    low=mid+1;
+14                }else{
+15                    return nums[mid];
+16                }
+17            }else{
+18                if(mid>=1 && nums[mid]==nums[mid-1]){
+19                    low=mid+1;
+20                }else if(mid<n-1 && nums[mid]==nums[mid+1]){
+21                    high=mid-1;
+22                }else{
+23                    return nums[mid];
+24                }
+25            }
+26       }
+27       return -1; 
+28    }
+29};
