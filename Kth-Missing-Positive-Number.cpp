924910fdff2d8df5e2156758c7b95d0d12d31429
muhdjasil <mohammedjass007@gmail.com>
2026-06-25 16:20:35 +0530

    [1539] [Time Beats: 100.00%] [Memory Beats: 92.56%] - LeetPush

--- Kth-Missing-Positive-Number.cpp
+++ Kth-Missing-Positive-Number.cpp
@@ -1,19 +1,17 @@
-class Solution {
-public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int low = 0;
-        int high = arr.size()-1;
-        int miss= -1;
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            if(arr[mid]-mid-1 < k){
-                miss = mid;
-                low = mid + 1;
-            }else{
-                high=mid-1;
-            }
-        }
-        if(miss==-1)return k;
-        return arr[miss]+k-arr[miss]+miss+1;
-    }
-};
+1class Solution {
+2public:
+3    int findKthPositive(vector<int>& arr, int k) {
+4        int low = 0;
+5        int high = arr.size()-1;
+6        while(low <= high){
+7            int mid = low + ((high-low)/2);
+8            int s = arr[mid] - (mid+1);
+9            if(s < k){
+10                low = mid+1;
+11            }else{
+12                high = mid-1;
+13            }
+14        }
+15        return k+high+1;
+16    }
+17};
