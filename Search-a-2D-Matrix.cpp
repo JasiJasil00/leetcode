5e1ec5ebb1f4688d5594898b1020a1b08c84b303
muhdjasil <mohammedjass007@gmail.com>
2026-06-28 18:50:00 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 44.35%] - LeetPush

--- Search-a-2D-Matrix.cpp
+++ Search-a-2D-Matrix.cpp
@@ -2,19 +2,22 @@
 2public:
 3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 4        int n = matrix.size();
-5        int m = matrix[0].size();
-6        int low = 0;
-7        int high = n*m - 1;
-8        while(low <= high){
-9            int mid = low + ((high-low)/2);
-10            if(matrix[mid/m][mid%m]< target){
-11                low = mid + 1;
-12            }else if( matrix[mid/m][mid%m] > target){
-13                high = mid - 1;
-14            }else{
-15                return true;
-16            }
-17        }
-18        return false;
-19    }
-20};
+5        for(int i = 0 ; i < n ; i++){
+6            int low = 0;
+7            int high = matrix[i].size()-1;
+8            if(matrix[i][low]<=target && matrix[i][high]>=target){
+9                while(low<=high){
+10                    int mid = (low+high)/2;
+11                    if(matrix[i][mid]==target)return true;
+12                    if(matrix[i][mid]>target){
+13                        high=mid-1;
+14                    }else{
+15                        low=mid+1;
+16                    }
+17                }
+18                return false;
+19            }
+20        }
+21        return false;
+22    }
+23};
