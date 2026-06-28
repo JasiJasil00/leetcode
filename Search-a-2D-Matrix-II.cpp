ccb86acf02eb913d055c4a9321b37a10ffeb0ad0
muhdjasil <mohammedjass007@gmail.com>
2026-06-28 18:56:47 +0530

    [240] [Time Beats: 81.56%] [Memory Beats: 38.06%] - LeetPush

--- Search-a-2D-Matrix-II.cpp
+++ Search-a-2D-Matrix-II.cpp
@@ -3,18 +3,16 @@
 3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 4        int n = matrix.size();
 5        int m = matrix[0].size();
-6        int row = 0;
+6        int row =0;
 7        int col = m-1;
-8        while(row < n && col >=0 ){
-9            if(matrix[row][col] < target ){
-10                row++;
-11            }else if(matrix[row][col] > target){
-12                col--;
-13            }else{
-14                return true;
-15            }
-16            
-17        }
-18        return false;
-19    }
-20};
+8        while(row<=n-1 && col>=0){
+9            if(matrix[row][col]==target)return true;
+10            if(matrix[row][col]>target){
+11                col--;
+12            }else{
+13                row++;
+14            }
+15        }
+16        return false;
+17    }
+18};
