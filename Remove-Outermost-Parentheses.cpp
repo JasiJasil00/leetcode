c2bb3909124c1f862d734c6dc96e99de666ba8a3
muhdjasil <mohammedjass007@gmail.com>
2026-06-30 15:14:07 +0530

    [1021] [Time Beats: 100.00%] [Memory Beats: 54.52%] - LeetPush

--- Remove-Outermost-Parentheses.cpp
+++ Remove-Outermost-Parentheses.cpp
@@ -1,18 +1,22 @@
 1class Solution {
 2public:
 3    string removeOuterParentheses(string s) {
-4        string result = "";
-5        int level = 0;
-6        for(char p : s){
-7            if(p == '('){
-8                if (level > 0) result += p;
-9                level++;
-10            }else if( p == ')'){
-11                 level--;  
-12                // If we're inside a primitive, add ')' to result
-13                if (level > 0) result += p;
-14            }
-15        }
-16        return result;
-17    }
-18};
+4        int count = 0;
+5        int n = s.size();
+6        string result= "";
+7        for(int i = 0 ; i < n ; i++ ){
+8            if(s[i]=='('){
+9                count++;
+10            }else {
+11                if(count==2){
+12                result+=s[i];
+13            }
+14                if(count>0)count--;
+15            }
+16             if(count>1){
+17                result+=s[i];
+18            }
+19        }
+20        return result;
+21    }
+22};
