79490abd972062110704a2e6c98472736cfa3666
muhdjasil <mohammedjass007@gmail.com>
2026-06-30 20:06:54 +0530

    [205] [Time Beats: 100.00%] [Memory Beats: 81.87%] - LeetPush

--- Isomorphic-Strings.cpp
+++ Isomorphic-Strings.cpp
@@ -1,24 +1,13 @@
 1class Solution {
 2public:
 3    bool isIsomorphic(string s, string t) {
-4            if(s.size()!=t.size())return false;
-5          map <char,char> hash;
-6          int n = s.size();
-7          for(int i = 0 ; i < n ; i++){
-8            if(hash.find(s[i])==hash.end()){
-9                hash[s[i]]=t[i];
-10            }else if(hash.find(s[i])!=hash.end() && hash[s[i]]!=t[i]){
-11                return false;
-12            }
-13          }  
-14          hash.clear();
-15          for(int i = 0 ; i < n ; i++){
-16            if(hash.find(t[i])==hash.end()){
-17                hash[t[i]]=s[i];
-18            }else if(hash.find(t[i])!=hash.end() && hash[t[i]]!=s[i]){
-19                return false;
-20            }
-21          }  
-22          return true;
-23    }
-24};
+4        vector<int> m1(256,0);
+5        vector<int> m2(256,0);
+6        for(int i = 0 ; i < s.size() ; i++){
+7            if(m1[s[i]]!=m2[t[i]])return false;
+8            m1[s[i]]=i+1;
+9            m2[t[i]]=i+1;
+10        }
+11        return true;
+12    }
+13};
