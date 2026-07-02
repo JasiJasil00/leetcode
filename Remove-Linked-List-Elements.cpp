7a8e520367b6118189a67c636707161812b2cbf2
muhdjasil <mohammedjass007@gmail.com>
2026-07-02 10:38:03 +0530

    [203] [Time Beats: 100.00%] [Memory Beats: 23.18%] - LeetPush

--- Remove-Linked-List-Elements.cpp
+++ Remove-Linked-List-Elements.cpp
@@ -1,18 +1,34 @@
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        ListNode dummy(0);         // Dummy node with any value
-        dummy.next = head;         // Point to the real head
-        ListNode* current = &dummy;
-
-        while (current->next != nullptr) {
-            if (current->next->val == val) {
-                current->next = current->next->next; // Skip the node
-            } else {
-                current = current->next;
-            }
-        }
-
-        return dummy.next; // New head (in case old head was deleted)
-    }
-};
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* removeElements(ListNode* head, int val) {
+14        if(head==NULL)return NULL;
+15        ListNode * prev = NULL;
+16        ListNode * temp = head;
+17        while(temp!=NULL){
+18            if(temp->val==val){
+19                ListNode * d = temp;
+20                if(prev==NULL){
+21                    head=temp->next;
+22                }else{
+23                    prev->next=temp->next;
+24                }
+25                temp= temp->next;
+26                delete d;
+27            }else{
+28                prev = temp;
+29                temp= temp->next;
+30            }
+31        }
+32        return head;
+33    }
+34};
