00b25ee25f832a0fdb80068fe8fbf135e6ebbffa
muhdjasil <mohammedjass007@gmail.com>
2026-07-02 14:45:54 +0530

    [234] [Time Beats: 100.00%] [Memory Beats: 53.25%] - LeetPush

--- Palindrome-Linked-List.cpp
+++ Palindrome-Linked-List.cpp
@@ -10,25 +10,36 @@
 10 */
 11class Solution {
 12public:
-13    bool isPalindrome(ListNode* head) {
-14        if(head==NULL && head->next==nullptr)return true;
-15        ListNode * temp = head;
-16        ListNode * slow = head;
-17        stack<ListNode*>s;
-18        while(temp!=NULL && temp->next!=NULL){
-19            s.push(slow);
-20            slow=slow->next;
-21            temp=temp->next->next;
-22        }
-23        if (temp != NULL) {
-24            slow = slow->next;
-25        }
-26        while(!s.empty()&&slow!=NULL){
-27            if(slow->val!=s.top()->val){
-28                return false;
-29            }s.pop();
-30                slow=slow->next;
-31        }
-32        return true;
-33    }
-34};
+13    ListNode* reverseList(ListNode* head) {
+14        ListNode * prev = NULL;
+15        ListNode * curr = head;
+16        while(curr!=NULL){
+17            ListNode * next = curr->next;
+18            curr->next = prev;
+19            prev = curr;
+20            curr = next;
+21        }
+22        return prev;
+23    }
+24    bool isPalindrome(ListNode* head) {
+25        if(head==NULL || head->next==NULL){
+26            return true;
+27        }
+28        
+29        ListNode * slow = head;
+30        ListNode * fast = head;
+31        while(fast!=NULL && fast->next!=NULL){
+32            slow = slow->next;
+33            fast = fast->next->next;
+34        }
+35        if(fast!=NULL)slow=slow->next;
+36        ListNode * newhead = reverseList(slow);
+37        slow = head;
+38        while(slow!=NULL && newhead!=NULL){
+39            if(newhead->val!=slow->val)return false;
+40            newhead=newhead->next;
+41            slow=slow->next;
+42        }
+43        return true;
+44    }
+45};
