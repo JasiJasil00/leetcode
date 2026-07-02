c0fed1758e632c50d1637836e7decf4793632442
muhdjasil <mohammedjass007@gmail.com>
2026-07-02 15:48:08 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 58.81%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.cpp
+++ Remove-Nth-Node-From-End-of-List.cpp
@@ -10,34 +10,24 @@
 10 */
 11class Solution {
 12public:
-13    void reverse(ListNode * &head){
-14        ListNode * prev = NULL;
-15        ListNode * curr = head;
-16        while(curr!=NULL){
-17            ListNode* next = curr->next;
-18            curr->next = prev;
-19            prev = curr;
-20            curr = next;
-21        }
-22        head = prev;
-23    }
-24    ListNode* removeNthFromEnd(ListNode* head, int n) {
-25        reverse(head);
-26        if(head==NULL)return NULL;
-27        ListNode * prev=NULL;
-28        ListNode* temp = head;
-29        int i =1;
-30        while(temp!=NULL && i<n){
-31            prev=temp;
-32            temp=temp->next;
-33            i++;
-34        }
-35        if(prev!=NULL){
-36            if(temp!=NULL)prev->next=temp->next;
-37        }else{
-38            head=head->next;
-39        }
-40        reverse(head);
-41        return head;
-42    }
-43};
+13    ListNode* removeNthFromEnd(ListNode* head, int n) {
+14        if(head==NULL )return NULL;
+15        ListNode *slow = head;
+16        ListNode * fast = head;
+17        for(int i = 0; i < n ; i++){
+18            fast = fast->next;
+19        }
+20        ListNode * prev = NULL;
+21        while(fast!=NULL){
+22            prev = slow;
+23            slow=slow->next;
+24            fast=fast->next;
+25        }
+26        ListNode * temp = slow;
+27        if(prev!=NULL)prev->next=slow->next;
+28        else head=head->next;
+29        delete temp;
+30        return head;
+31
+32    }
+33};
